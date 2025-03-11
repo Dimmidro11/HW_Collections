f1be00777baa02f2026c16af47e4237535413d0d
Dimmidroll <diman15061997@mail.ru>
2025-03-11 10:33:04 +0500

    Quest2 done

--- src/main/java/ru/netology/javaqa/Game.java
+++ src/main/java/ru/netology/javaqa/Game.java
@@ -1,37 +1,32 @@
 package ru.netology.javaqa;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game {
 
-    ArrayList<Player> players = new ArrayList<>();
+    HashMap<String, Integer> players = new HashMap<>();
 
-    public void register (Player player) {
-        players.add(player);
+    public void register(Integer strength, String name) {
+        players.put(name, strength);
     }
 
     public int round(String playerName1, String playerName2) {
-        if (searchByName(playerName1) == null) {
-            throw new NotRegisteredException("Player with name " + playerName1 + " not found");
-        }
-        if (searchByName(playerName2) == null) {
-            throw new NotRegisteredException("Player with name " + playerName2 + " not found");
-        }
-        if (searchByName(playerName1).getStrength() < searchByName(playerName2).getStrength()) {
+        if (searchByName(playerName1) < searchByName(playerName2)) {
             return 2;
-        } else if (searchByName(playerName1).getStrength() > searchByName(playerName2).getStrength()) {
+        } else if (searchByName(playerName1) > searchByName(playerName2)) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    public Player searchByName(String name) {
-        for (Player player : players) {
-            if (name.equals(player.getName())) {
-                return player;
+    public Integer searchByName(String name) {
+        for (String key : players.keySet()) {
+            if (key.equals(name)) {
+                return players.get(name);
             }
         }
-        return null;
+        throw new NotRegisteredException("Player with name " + name + " not found");
     }
 }
+
--- src/test/java/ru/netology/javaqa/GameTest.java
+++ src/test/java/ru/netology/javaqa/GameTest.java
@@ -9,34 +9,32 @@ import java.util.ArrayList;
 public class GameTest {
 
     Game game = new Game();
-    Player roman = new Player(1, "Роман", 10);
-    Player artem = new Player (2, "Артем",25);
-    Player vladislav = new Player (3, "Владислав",40);
-    Player dmitry = new Player (4, "Дмитрий",35);
-    Player ekaterina = new Player (5, "Екатерина",20);
-    Player irina = new Player (6, "Ирина",15);
-    Player vera = new Player (7, "Вера",13);
-    Player ulyana = new Player (8, "Ульяна",10);
 
     @BeforeEach
     public void setup() {
-        game.register(roman);
-        game.register(artem);
-        game.register(vladislav);
-        game.register(dmitry);
-        game.register(ekaterina);
-        game.register(irina);
-        game.register(vera);
-        game.register(ulyana);
+        game.register(10, "Роман");
+        game.register(25, "Артем");
+        game.register(40, "Владислав");
+        game.register(35, "Дмитрий");
+        game.register(20, "Екатерина");
+        game.register(15, "Ирина");
+        game.register(13, "Вера");
+        game.register(10, "Ульяна");
     }
 
     @Test
-    public void shouldFindByName() {
-
-        Player expected = vladislav;
-        Player actual = game.searchByName("Владислав");
+    public void shouldFindPlayer() {
+        Integer expected = 10;
+        Integer actual = game.searchByName("Роман");
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindPlayer() {
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.searchByName("Петр");
+        });
     }
 
     @Test
